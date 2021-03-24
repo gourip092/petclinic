28ea66aa3bcc857661da7a42fb4f7f4cbeb2611e
gourip092 <74867564+gourip092@users.noreply.github.com>
2021-03-24 21:46:09 +0530

    Update smoketest.sh

--- smoketest.sh
+++ smoketest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ADDRESSES=(
-    'http://13.235.91.150:8080/petclinic/' #Here you insert addresses to test :D
+    'http://13.235.91.150:8080/petclinic/' #Here you insert addresses to test
 )
 
 function checkAddress
